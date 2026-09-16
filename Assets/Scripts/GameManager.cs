ba56711fe3234b2a59777bb0bc76c1d4d283a6d2
mage120 <parkhonchan@gmail.com>
2026-09-16 16:06:22 +0900

    유니티 프로젝트 최초 등록

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    int a = 0;
+    void Start()
+    {
+        Debug.Log(a);
+        Debug.Log("HelloWorld!");
+    }
+}
